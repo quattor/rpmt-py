e49d9ca31375663fccac7eb5f826bab4cdb3eaba
ctria <ctria@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2011-02-25 14:25:07 +0000

    2011-02-25  Christos Triantafyllidis,ctria@grid.auth.gr
    	* Release: 1.0.4
    	- Added support for modern distributions (RHEL6, Fedora etc).
    
    
    
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/rpmt-py@2568 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- config.mk
+++ config.mk
@@ -6,7 +6,7 @@ COMP=rpmt-py
 
 NAME=$(COMP)
 DESCR=RPMT - RPM Transactional (Python version)
-VERSION=1.0.3
+VERSION=1.0.4
 RELEASE=1
 
 AUTHOR=Nicolas Pinto <Nicolas.Pinto@cern.ch>
@@ -17,4 +17,4 @@ RPMT_CACHE=/var/cache/rpmt
 
 MANSECT=8
 
-DATE=17/10/08 15:04
+DATE=25/02/11 16:24
